dac7d343133c300f800318eaf5561e75f54b09c6
lqasemzadeh <lqasemzadeh@gmail.com>
2024-06-08 18:38:55 +0330

    Update testimonials content in Content.js
    The testimonials_content within the Content.js file has been updated. Placeholder texts have been removed, and real contents including names and associated URLs have been added. Multiple content entries have been reduced to two real-world examples.

--- src/Content.js
+++ src/Content.js
@@ -153,28 +153,14 @@ export const content = {
     subtitle: "MY CLIENT REVIEWS",
     testimonials_content: [
       {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
         img: avatar1,
-        name: "JOHN DOE",
+        review: "https://steelarvintav.com/",
+        name: "Steel ArvinTav Indutrial Company",
       },
       {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
         img: avatar2,
-        name: "Tom Alex",
-      },
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar3,
-        name: "Johnny",
-      },
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar4,
-        name: "ROBBIN",
+        review: "https://setaregan.co/",
+        name: "Setaregan Computer Company",
       },
     ],
   },
