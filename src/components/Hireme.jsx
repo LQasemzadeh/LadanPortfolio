c57e1eec94906200754b648988a75c3fa773e538
lqasemzadeh <lqasemzadeh@gmail.com>
2024-06-10 17:54:44 +0330

    Update testimonials content in Content.js
    The testimonials_content within the Content.js file has been updated. Placeholder texts have been removed, and real contents including names and associated URLs have been added. Multiple content entries have been reduced to two real-world examples.

--- src/components/Hireme.jsx
+++ src/components/Hireme.jsx
@@ -7,17 +7,18 @@ const Hireme = () => {
 
   return <section className="bg-bg-light-primary">
     <div className="md:container px-5 pt-14">
-      <h2 className="title">{Hireme.title}</h2>
-      <h4 className="subtitle">{Hireme.subtitle}</h4>
+      <h2 className="title" data-aos="fade-down">{Hireme.title}</h2>
+      <h4 className="subtitle" data-aos="fade-down">{Hireme.subtitle}</h4>
       <br />
       <div className="flex items-center md:flex-row flex-col-reverse">
         <img
         src={Hireme.image1}
+        data-aos="fade-right"
         alt=""
         className="max-w-sm md:block hidden"
         />
-        <img src={Hireme.image2} alt="" className="max-w-sm md:hidden" />
-        <div className="border-2 border-dark_primary max-w-sm p-6 shadow-sm rounded-xl rounded-br-[8rem]
+        <img src={Hireme.image2} data-aos="fade-up" alt="" className="max-w-sm md:hidden" />
+        <div data-aos="fade-left" className="border-2 border-dark_primary max-w-sm p-6 shadow-sm rounded-xl rounded-br-[8rem]
         sm:min-w-[22rem]">
           <p className="leading-7">{Hireme.para}</p>
           <br/>
