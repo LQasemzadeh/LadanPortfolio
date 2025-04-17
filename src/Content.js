e8bf5d2a3626898d638b00e8b68c65c1c2023b38
lqasemzadeh <lqasemzadeh@gmail.com>
2025-04-17 12:35:47 +0200

    Fix incorrect paths for project3 image imports
    Corrected the file paths for the project3 image in both `Projects.jsx` and `Content.js` to ensure proper case sensitivity alignment. This resolves potential issues in environments with case-sensitive file systems.

--- src/Content.js
+++ src/Content.js
@@ -11,7 +11,7 @@ import Next from "./assets/images/Skills/Next.webp";
 
 import project1 from "./assets/images/Projects/steelarvin.png";
 import project2 from "./assets/images/Projects/setaregan.png";
-import project3 from "./assets/images/projects/FaFa1.png";
+import project3 from "./assets/images/Projects/FaFa1.png";
 import person_project from "./assets/images/projects/ladan2.webp";
 
 import steelarvin from "./assets/images/Testimonials/steelarvin.png";
